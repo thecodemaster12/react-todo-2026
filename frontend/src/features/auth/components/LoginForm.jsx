09ed39a12eb03ea8d78a9971ae16882ed779b7f5
thecodemaster12 <thecodemaster26@gmail.com>
2026-04-05 22:53:49 +0600

    Login form

--- frontend/src/features/auth/components/LoginForm.jsx
+++ frontend/src/features/auth/components/LoginForm.jsx
@@ -1,36 +1,77 @@
-import React from "react";
+import React, { useState } from "react";
 
 const LoginForm = () => {
+  const [form, setForm] = useState({
+    email: "",
+    password: "",
+  });
+
+  const [errors, setErrors] = useState({});
+
+  const handleChange = (e) => {
+    setForm({
+      ...form,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const validate = () => {
+    const newErrors = {};
+    if (!form.email.trim()) {
+      newErrors.email = "Email is required";
+    } else if (!/\S+@\S+\.\S+/.test(form.email)) {
+      newErrors.email = "Invalid email format";
+    }
+
+    if (!form.password.trim()) {
+      newErrors.password = "Password is required";
+    } else if (form.password.length < 6)
+      newErrors.password = "Password must be at least 6 characters";
+
+    return newErrors;
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    const validationErrors = validate();
+
+    if (Object.keys(validationErrors).length > 0) {
+      setErrors(validationErrors);
+      return;
+    }
+
+    setErrors({});
+
+    alert("Good");
+  };
+
   return (
-    <form className="space-y-4">
-      {/* Email */}
+    <form onSubmit={handleSubmit} className="space-y-4">
       <div>
         <input
           className="border p-2 w-full"
           type="email"
           name="email"
-          // value={form.email}
+          value={form.email}
           placeholder="Email"
-          // onChange={handleChange}
+          onChange={handleChange}
         />
-        {/* {errors.email && (
-          <p className="text-red-500 text-sm">{errors.email}</p>
-        )} */}
+        {errors.email && <p className="text-red-500 text-sm">{errors.email}</p>}
       </div>
 
-      {/* Password */}
       <div>
         <input
           className="border p-2 w-full"
           type="password"
           name="password"
-          // value={form.password}
+          value={form.password}
           placeholder="Password"
-          // onChange={handleChange}
+          onChange={handleChange}
         />
-        {/* {errors.password && (
+        {errors.password && (
           <p className="text-red-500 text-sm">{errors.password}</p>
-        )} */}
+        )}
       </div>
 
       <div className="text-center">
@@ -40,7 +81,9 @@ const LoginForm = () => {
       </div>
 
       <div className="text-center">
-        <a className="text-blue-500 italic" href="#">Register ?</a>
+        <a className="text-blue-500 italic" href="/register">
+          Register ?
+        </a>
       </div>
     </form>
   );
